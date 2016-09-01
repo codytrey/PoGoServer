b78b3136eb39ee6d13864320397522e65fcc8546
codytrey <cody.t.belcher@gmail.com>
2016-08-31 21:31:09 -0500

    adding base login URL

--- v1/admin.py
+++ v1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Player, BuddyPokemon, Currency, EquippedBadge, DailyBonus, PlayerAvatar
+
+admin.site.register(Player)
+admin.site.register(BuddyPokemon)
+admin.site.register(Currency)
+admin.site.register(EquippedBadge)
+admin.site.register(DailyBonus)
+admin.site.register(PlayerAvatar)
