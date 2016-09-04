529b2ab1fdbb71410461347c2ead38f66bc279a5
Cody Belcher (codytrey) <cody.t.belcher@gmail.com>
2016-09-04 09:33:40 -0500

    added protobuf files

--- v1/views.py
+++ v1/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from POGOProtos.Networking.Envelopes_pb2 import RequestEnvelope
+from POGOProtos.Networking.Envelopes_pb2 import ResponseEnvelope
+from POGOProtos.Networking.Requests_pb2 import RequestType
+from POGOProtos.Networking.Requests import Messages_pb2
+from google.protobuf import message
+
 # Create your views here.
 def login(request):
+    mesg = Messages_pb2(request.body)
     return HttpResponse("Hi, you're trying to login.")
 
 def dumbdumb(request):
